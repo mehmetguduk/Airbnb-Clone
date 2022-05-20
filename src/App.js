6e40d00ec1954c365a1122d480e773e3dcd8898b
mehmetguduk <mehmetguduk98@gmail.com>
2022-05-20 18:04:36 +0300

    master

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Cards from "./components/Cards"
+import Footer from "./components/Footer"
 
 export default function App() {
     return (
@@ -14,6 +15,7 @@ export default function App() {
             <Navbar/>
             <Hero/>
             <Cards/>
+            <Footer/>
         </div>
     );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+/*
+    THIS FILE WAS DEVELOPED BY MEHMET GUDUK
+    © 2022 COPYRIGHT, LICENSED WITH GPL-3.0 LICENSE, AUTHOR IS MEHMET GUDUK
+    https://github.com/mehmetguduk
+*/
+
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <p class="footer-copyright">
+                © 2022 Copyright <a class="copyright-link" href="https://github.com/mehmetguduk" target="_blank" rel="noreferrer">Mehmet Güdük</a> All Rights Reserved
+            </p>
+        </footer>
+    )
+}
